65ba4d9d7d5b79b3b56d432e029464f12c9f9cf6
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2023-03-31 13:00:14 +0800

    Add files via upload

--- locales/tw.lua
+++ locales/tw.lua
@@ -1,1 +1,14 @@
-
+Locales['tw'] = {
+    ['washing_count'] = '黑錢金額',
+    ['washing_money'] = '洗錢中...',
+    ['wash_fail']     = '洗錢失敗',
+    ['no_enough_black'] = '無足夠黑錢',
+    ['count_empty']     = '請輸入金額',
+    ['press_to_open']   = '~g~[E]~s~ 洗錢點 - %s',
+    ['moneywash']       = '洗錢 - %s',
+    -- server
+    ['washed_money'] = '清洗黑錢 $%s, 獲得 %s',
+    ['cheating'] = '疑似使用外掛```\nSteam名字: %s\n角色姓名 %s\nSteam: %s\n時間: %s```<@%s>',
+    ['mechanism'] = '嘗試使用機制```\nSteam名字: %s\n角色姓名 %s\nSteam: %s\n時間: %s```<@%s>',
+    ['defaultLog'] = '玩家: %s [%s]\n角色姓名: %s\n黑錢: %s\n現金: %s\n地點 %s\n時間: %s'
+}
